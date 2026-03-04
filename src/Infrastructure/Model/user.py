6eb1ab529314dbbc1b67f02bf941a711f82b6062
Anderson Martins Alberissi <2403321@SALAS.AULAS>
2026-03-03 21:30:16 -0300

    refactor: Arrumar tabela banco de dados

--- src/Infrastructure/Model/user.py
+++ src/Infrastructure/Model/user.py
@@ -4,12 +4,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    cnpj = db.Column(db.String(14), nullable=False)
+    celular = db.Column(db.String(11), nullable=False)
+    status = db.Column(db.Boolean, default=False, nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "cnpj": self.cnpj,
             "email": self.email,
-            "password": self.password
+            "celular": self.celular,
+            "password": self.password,
+            "status": self.status
         }
+
