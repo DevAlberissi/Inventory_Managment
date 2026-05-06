d93045accc1256e852c6a66d111d17d2cb4602b9
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-05-05 22:33:27 -0300

    feat: criar rota base para os dashboards

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,6 +3,7 @@ from src.routes.user_routes import user_bp
 from src.routes.product_routes import products_bp
 from src.routes.documento_routes import documentos_bp
 from src.routes.venda_routes import venda_bp
+from src.routes.dashboard_routes import dashboard_bp
 
 def init_routes(app):
     app.register_blueprint(auth_bp)
@@ -10,3 +11,4 @@ def init_routes(app):
     app.register_blueprint(products_bp)
     app.register_blueprint(documentos_bp)
     app.register_blueprint(venda_bp)
+    app.register_blueprint(dashboard_bp)
