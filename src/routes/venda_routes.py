3b5a303fd3df4b0cd8395b00e5156685b537e74a
Anderson Alberissi <aalberissi@gmail.com>
2026-05-07 20:43:36 -0300

    fix: corrigir nome da variável do blueprint de vendas

--- src/routes/venda_routes.py
+++ src/routes/venda_routes.py
@@ -2,27 +2,27 @@ from flask import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from src.Application.Controllers.venda_controller import VendaController
 
-vendas_bp = Blueprint('vendas', __name__, url_prefix='/vendas')
+venda_bp = Blueprint('vendas', __name__, url_prefix='/vendas')
 
-@vendas_bp.route('', methods=['POST'])
+@venda_bp.route('', methods=['POST'])
 @jwt_required()
 def create_venda():
     seller_id = int(get_jwt_identity())
     return VendaController.create_venda(seller_id)
 
-@vendas_bp.route('', methods=['GET'])
+@venda_bp.route('', methods=['GET'])
 @jwt_required()
 def list_vendas():
     seller_id = int(get_jwt_identity())
     return VendaController.list_vendas(seller_id)
 
-@vendas_bp.route('/<int:venda_id>', methods=['GET'])
+@venda_bp.route('/<int:venda_id>', methods=['GET'])
 @jwt_required()
 def get_venda(venda_id):
     seller_id = int(get_jwt_identity())
     return VendaController.get_venda(seller_id, venda_id)
 
-@vendas_bp.route('/<int:venda_id>', methods=['DELETE'])
+@venda_bp.route('/<int:venda_id>', methods=['DELETE'])
 @jwt_required()
 def delete_venda(venda_id):
     seller_id = int(get_jwt_identity())
